1e3d6b4522c6981e21072bcc940781f0aff75945
NickPepper <nickpeppper@gmail.com>
2016-04-25 14:26:33 +0300

    omg

--- Kumar/main.cpp
+++ Kumar/main.cpp
@@ -10,47 +10,82 @@ SDL_Surface* screenSurface  = NULL;
 SDL_Rect display_bounds;
 
 
-int getVideoInfo()
+int getPlatformAndCPUInformation()
 {
-    int i, display_mode_count;
-    SDL_DisplayMode mode;
-    Uint32 f;    
+    printf("\n");
+    
+    /*
+    If the correct platform name is not available, returns a string beginning with the text "Unknown".
+    Here are the names returned for some supported platforms:
+        Windows
+        Mac OS X
+        Linux
+        iOS
+        Android
+    */
+    SDL_Log("Platform: %s", SDL_GetPlatform());
 
-    // get available displays and their modes
-    SDL_Log("SDL_GetNumVideoDisplays(): %i", SDL_GetNumVideoDisplays());
-    display_mode_count = SDL_GetNumDisplayModes(display_in_use);
-    if (display_mode_count < 1) {
-        SDL_Log("SDL_GetNumDisplayModes failed: %s", SDL_GetError());
-        return 1;
-    }
-    SDL_Log("SDL_GetNumDisplayModes: %i", display_mode_count);
-    for (i = 0; i < display_mode_count; ++i) {
-        if (SDL_GetDisplayMode(display_in_use, i, &mode) != 0) {
-            SDL_Log("SDL_GetDisplayMode failed: %s", SDL_GetError());
-            return 1;
-        }
-        f = mode.format;
+    // RAM amount
+    SDL_Log("RAM: %d MB", SDL_GetSystemRAM());
 
-        SDL_Log("Mode %i\tbpp %i\t%s\t%i x %i", i,
-        SDL_BITSPERPIXEL(f), SDL_GetPixelFormatName(f), mode.w, mode.h);
-    }
+    ///////////////////////////////////////////////////////////
+    // TODO: OS name (!)
+    // TODO: processor model (!)
+    // TODO: byte order (!)
+    // TODO: etc info (!)
+    ///////////////////////////////////////////////////////////
 
-    // get display bounds
-    if (SDL_GetDisplayBounds(display_in_use, &display_bounds) == 0) {
-        SDL_Log("Display Bounds: x:%d, y:%d, width:%d, height:%d", 
-                display_bounds.x, display_bounds.y, display_bounds.w, display_bounds.h);
-    } else {
-        SDL_Log("SDL_GetDisplayBounds failed: %s", SDL_GetError());
-        return 1;
-    }
+    // number of logical cores
+    SDL_Log("Logical CPU cores: %d", SDL_GetCPUCount());
 
-    // get all video drivers info
+    // useful for determining multi-threaded structure padding or SIMD prefetch sizes
+    SDL_Log("CPU's L1 cache line size: %d KB", SDL_GetCPUCacheLineSize());
+
+    // 3DNow! features support ?
+    SDL_Log("3DNow! support: %s", SDL_Has3DNow()?"YEP":"NOPE");
+
+    // AVX features support ?
+    SDL_Log("AVX support: %s", SDL_HasAVX()?"YEP":"NOPE");
+
+    // AVX2 features support ?
+    SDL_Log("AVX2 support: %s", SDL_HasAVX2()?"YEP":"NOPE");
+
+    // AltiVec features support ?
+    SDL_Log("AltiVec support: %s", SDL_HasAltiVec()?"YEP":"NOPE");
+
+    // MMX features support ?
+    SDL_Log("MMX support: %s", SDL_HasMMX()?"YEP":"NOPE");
+
+    // RDTSC instruction support?
+    SDL_Log("RDTSC support: %s", SDL_HasRDTSC()?"YEP":"NOPE");
+
+    // SSE features support?
+    SDL_Log("SSE support: %s", SDL_HasSSE()?"YEP":"NOPE");
+
+    // SSE2 features support?
+    SDL_Log("SSE2 support: %s", SDL_HasSSE2()?"YEP":"NOPE");
+
+    // SSE3 features support?
+    SDL_Log("SSE3 support: %s", SDL_HasSSE3()?"YEP":"NOPE");
+
+    // SSE4.1 features support?
+    SDL_Log("SSE41 support: %s", SDL_HasSSE41()?"YEP":"NOPE");
+
+    // SSE4.2 features support?
+    SDL_Log("SSE42 support: %s", SDL_HasSSE42()?"YEP":"NOPE");
+
+    return 0;
+}
+
+
+int getAllVideoDriversInfo()
+{
+    printf("\n");
     int vdrivers_num = SDL_GetNumVideoDrivers();
     SDL_Log("Number of Video Drivers: %d", vdrivers_num);
     for(int i = 0; i < vdrivers_num; ++i) {
         SDL_Log("Video Driver %d: %s", i, SDL_GetVideoDriver(i));
     }
-
     // get current video driver info
     const char* curr_video_driver = SDL_GetCurrentVideoDriver();
     if( curr_video_driver != NULL ) {
@@ -59,21 +94,67 @@ int getVideoInfo()
         SDL_Log("SDL_GetCurrentVideoDriver failed: %s", SDL_GetError());
         return 1;
     }
+    return 0;
+}
+
+
+int getAvailableDisplaysAndTheirModes()
+{
+    int display_mode_count;
+    SDL_DisplayMode mode;
+    Uint32 f;    
+
+    printf("\n");
+
+    SDL_Log("SDL_GetNumVideoDisplays(): %i", SDL_GetNumVideoDisplays());
+    display_mode_count = SDL_GetNumDisplayModes(display_in_use);
+    if (display_mode_count < 1) {
+        SDL_Log("SDL_GetNumDisplayModes failed: %s", SDL_GetError());
+        return 1;
+    }
+    SDL_Log("SDL_GetNumDisplayModes: %i", display_mode_count);
+    for (int i = 0; i < display_mode_count; ++i) {
+        if (SDL_GetDisplayMode(display_in_use, i, &mode) != 0) {
+            SDL_Log("SDL_GetDisplayMode failed: %s", SDL_GetError());
+            return 1;
+        }
+        f = mode.format;
+
+        SDL_Log("Mode %i\tbpp %i\t%s\t%i x %i", 
+                    i, SDL_BITSPERPIXEL(f), SDL_GetPixelFormatName(f), mode.w, mode.h);
+    }
 
     return 0;
 }
 
+
+int getDisplayBounds()
+{
+    printf("\n");
+    if (SDL_GetDisplayBounds(display_in_use, &display_bounds) == 0) {
+        SDL_Log("Current Display Bounds: x:%d, y:%d, width:%d, height:%d", 
+                    display_bounds.x, display_bounds.y, display_bounds.w, display_bounds.h);
+    } else {
+        SDL_Log("SDL_GetDisplayBounds failed: %s", SDL_GetError());
+        return 1;
+    }
+    return 0;
+}
 
 
 int main(/*int argc, char *argv[]*/)
 {
     if( SDL_Init( SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_AUDIO ) < 0 )
     {
-        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
         return 1;
     }
 
-    DEBUG && getVideoInfo();
+
+    DEBUG && getPlatformAndCPUInformation();
+    DEBUG && getAllVideoDriversInfo();
+    DEBUG && getAvailableDisplaysAndTheirModes();
+    DEBUG && getDisplayBounds();
 
 
     window = SDL_CreateWindow( WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
@@ -153,7 +234,7 @@ int main(/*int argc, char *argv[]*/)
         //SDL_DisableScreenSaver();
     } 
     else {
-        SDL_Log( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+        SDL_Log("Window could not be created! SDL_Error: %s\n", SDL_GetError());
         return 1;
     }
 
